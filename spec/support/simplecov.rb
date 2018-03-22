5069e3af6bc4e4e6814d19e8d84bbe75dc24cf85
Pavol Zbell <pavol.zbell@gmail.com>
2018-03-22 14:52:27 +0100

    Do not rely on CIRCLE_ARTIFACTS

--- spec/support/simplecov.rb
+++ spec/support/simplecov.rb
@@ -1,13 +1,5 @@
 require 'simplecov'
 
 SimpleCov.start do
-  if ENV['CIRCLE_ARTIFACTS']
-    coverage_dir File.join(ENV['CIRCLE_ARTIFACTS'], 'coverage')
-  else
-    coverage_dir File.join(Rails.root, 'tmp', 'coverage')
-  end
+  coverage_dir File.join(Rails.root, 'tmp', 'coverage')
 end
-
-puts "----"
-puts File.join(ENV['CIRCLE_ARTIFACTS'], 'coverage')
-puts "----"
