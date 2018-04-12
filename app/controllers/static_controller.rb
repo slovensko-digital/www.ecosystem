e4ca4f4a8a934c4dba9e8532e3d0a378c376a6c1
Ernest Walzel <ewalzel@gmail.com>
2018-04-12 13:34:29 +0200

    Add price lists

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -3,4 +3,8 @@ class StaticController < ContentController
     @page.title = 'Podmienky používania &middot; Ekosystém.Slovensko.Digital'.html_safe
     @page.og.description = ' Licenciu sme nastavili tak, aby boli podmienky používania úplne minimálne a mohli s dátami v princípe robiť čo chcete. Detailné podmienky v právničtine odporúča prečítať 10 z 10 právnikov.'
   end
+
+  def pricing
+    @page.title = 'Cenník služieb &middot; Ekosystém.Slovensko.Digital'.html_safe
+  end
 end
