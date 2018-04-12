8bcd3222af939e8bbe773e9a7224946af93260a9
Ernest Walzel <ewalzel@gmail.com>
2018-04-12 15:20:35 +0200

    Separate pages for dataset & services T&Cs

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,7 +1,11 @@
 class StaticController < ContentController
-  def terms
-    @page.title = 'Podmienky používania &middot; Ekosystém.Slovensko.Digital'.html_safe
-    @page.og.description = ' Licenciu sme nastavili tak, aby boli podmienky používania úplne minimálne a mohli s dátami v princípe robiť čo chcete. Detailné podmienky v právničtine odporúča prečítať 10 z 10 právnikov.'
+  def datasets_terms
+    @page.title = 'Podmienky používania datasetov &middot; Ekosystém.Slovensko.Digital'.html_safe
+    @page.og.description = 'Licenciu sme nastavili tak, aby boli podmienky používania úplne minimálne a mohli s dátami v princípe robiť čo chcete. Detailné podmienky v právničtine odporúča prečítať 10 z 10 právnikov.'
+  end
+
+  def services_terms
+    @page.title = 'Podmienky používania služieb &middot; Ekosystém.Slovensko.Digital'.html_safe
   end
 
   def pricing
