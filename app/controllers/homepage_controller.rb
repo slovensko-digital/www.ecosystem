2e6e203134072ce58039410444e6a9798b3296cc
michalrohacek256 <michal.rohacek.256@gmail.com>
2020-07-15 01:21:21 +0200

    Refactor

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -1,33 +1,29 @@
 class HomepageController < ContentController
+  include ActionView::Helpers::NumberHelper
+
   before_action :fetch_dump_sizes, only: :index
+
+  DUMPS_URL = 'https://s3.eu-central-1.amazonaws.com/ekosystem-slovensko-digital-dumps/'
 
   def index
     @page.title = 'Služby. Otvorené dáta &amp; API. &middot; Ekosystém.Slovensko.Digital'.html_safe
     @page.og.title = 'Služby. Otvorené dáta &amp; API.'.html_safe
     @page.og.description = 'Lepšie digitálne služby štátu nie sú sci-fi.'
+    @url = DUMPS_URL
   end
 
   private
 
   def fetch_dump_sizes
-    uri = URI('https://s3.eu-central-1.amazonaws.com/ekosystem-slovensko-digital-dumps/')
+    uri = URI(DUMPS_URL)
 
     @dump_sizes = Rails.cache.fetch('dump_sizes') do
-      DEFAULT_DUMP_SIZES.map do |schema_name, default_size|
-        size = Net::HTTP.start(uri.host) { |http| (Integer(http.request_head(URI.join(uri, "#{schema_name}.sql.gz"))['content-length']) / 1_000_000).ceil rescue default_size }
-        [schema_name, "(#{size} MB)"]
+      SCHEMAS.map do |schema_name|
+        size = Net::HTTP.start(uri.host) { |http| Integer(http.request_head(URI.join(uri, "#{schema_name}.sql.gz"))['content-length']) rescue nil }
+        [schema_name, size.present? ? "(#{number_to_human_size(size, precision: 2, separator: ',')})" : '(? MB)']
       end.to_h
     end
   end
 
-  DEFAULT_DUMP_SIZES = {
-    rpo: 1500,
-    itms: 420,
-    vvo: 460,
-    ov: 2400,
-    ruz: 110,
-    socpoist: 5,
-    vszp: 6,
-    crz: 320
-  }
+  SCHEMAS = [:rpo, :itms, :vvo, :ov, :ruz, :socpoist, :vszp, :crz]
 end
