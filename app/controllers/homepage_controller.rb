34519eadb0caec96a60b6c3085e182dca45a13ab
michalrohacek256 <michal.rohacek.256@gmail.com>
2020-07-15 11:40:42 +0200

    Refactor controller

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -1,29 +1,28 @@
 class HomepageController < ContentController
   include ActionView::Helpers::NumberHelper
 
-  before_action :fetch_dump_sizes, only: :index
+  before_action :dump_metadata, only: :index
 
-  DUMPS_URL = 'https://s3.eu-central-1.amazonaws.com/ekosystem-slovensko-digital-dumps/'
+  SCHEMAS = [:crz, :itms, :ov, :rpo, :ruz, :socpoist, :vszp, :vvo]
 
   def index
     @page.title = 'Služby. Otvorené dáta &amp; API. &middot; Ekosystém.Slovensko.Digital'.html_safe
     @page.og.title = 'Služby. Otvorené dáta &amp; API.'.html_safe
     @page.og.description = 'Lepšie digitálne služby štátu nie sú sci-fi.'
-    @url = DUMPS_URL
   end
 
   private
 
-  def fetch_dump_sizes
-    uri = URI(DUMPS_URL)
+  def dump_metadata
+    @dump_urls = SCHEMAS.map do |schema|
+      [schema, URI('https://s3.eu-central-1.amazonaws.com/ekosystem-slovensko-digital-dumps/').merge("#{schema}.sql.gz")]
+    end.to_h
 
     @dump_sizes = Rails.cache.fetch('dump_sizes') do
-      SCHEMAS.map do |schema_name|
-        size = Net::HTTP.start(uri.host) { |http| Integer(http.request_head(URI.join(uri, "#{schema_name}.sql.gz"))['content-length']) rescue nil }
-        [schema_name, size.present? ? "(#{number_to_human_size(size, precision: 2, separator: ',')})" : '(? MB)']
+      SCHEMAS.map do |schema|
+        size = Net::HTTP.start(@dump_urls[schema].host) { |http| Integer(http.request_head(@dump_urls[schema])['content-length']) rescue nil }
+        [schema, size.present? ? "(#{number_to_human_size(size, precision: 2, separator: ',')})" : '(? MB)']
       end.to_h
     end
   end
-
-  SCHEMAS = [:rpo, :itms, :vvo, :ov, :ruz, :socpoist, :vszp, :crz]
 end
