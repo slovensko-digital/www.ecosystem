da16c8be4899dd00d08b51deb8335559652f426b
michal-rohacek <michal.rohacek.256@gmail.com>
2020-10-13 03:37:59 +0200

    Simplify rollbar error reporting

--- app/controllers/services/govbox_controller.rb
+++ app/controllers/services/govbox_controller.rb
@@ -80,7 +80,7 @@ class Services::GovboxController < ContentController
     @page.title = 'Cenník &middot; GovBox'.html_safe
     @page.og.image = view_context.image_url('services/govbox/facebook_share.png')
   end
-
+  SocketError
   private
 
   def ensure_live
@@ -93,7 +93,7 @@ class Services::GovboxController < ContentController
     response = RestClient::Resource.new("#{ENV.fetch('AUTOFORM_URL')}/api/corporate_bodies/search", timeout: 10).get(params: { q: "cin:#{params[:cin]}", private_access_token: ENV.fetch('AUTOFORM_PRIVATE_ACCESS_TOKEN') })
     JSON.parse(response.body, symbolize_names: true).first&.fetch(:statutory)
   rescue RestClient::ExceptionWithResponse => e
-    Rollbar.error(e, "GB registration: Autoform request failed for CIN #{params['cin']}, subject name #{params['legal_subject_name']}: HTTP status code #{e.http_code}")
+    Rollbar.error(e, params)
     nil
   end
 end
