a6b6bb44829bc2395b78dc169ed27ce3450b03a0
luciajanikova <19lucia99@gmail.com>
2022-02-15 19:55:10 +0100

    Show GovBox API as a separate project

---
+++ app/controllers/services/govbox_api_controller.rb
@@ -0,0 +1,24 @@
+class Services::GovboxApiController < ContentController
+  layout 'application_bs4'
+
+  before_action :ensure_live, only: [:index]
+
+    def index
+    @page.title = 'Správy zo slovensko.sk integrované na interný systém &middot; GovBox API'.html_safe
+    @page.og.image = view_context.image_url('services/govbox/facebook_share.png')
+    @page.og.description = 'Ideálne riešenie pre firmy, ktoré intenzívne využívajú schránku na slovensko.sk, potrebujú zasielať hromadné podania alebo uvažujú o automatizácii procesov. Od Slovensko.Digital.'
+  end
+
+  def terms
+    @page.title = 'Všeobecné obchodné podmienky &middot; GovBox API'.html_safe
+    @page.og.image = view_context.image_url('services/govbox/facebook_share.png')
+  end
+
+  private
+
+  def ensure_live
+    unless rollout.active?(:govbox)
+      render template: 'errors/not_found', status: 404, layout: 'application'
+    end
+  end
+end
