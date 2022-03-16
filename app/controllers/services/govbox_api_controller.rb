5f06ee9eb93bbe936047314375399ca0060c3002
luciajanikova <19lucia99@gmail.com>
2022-03-16 22:46:54 +0100

    Fix govbox-api controller after templates update

--- app/controllers/services/govbox_api_controller.rb
+++ app/controllers/services/govbox_api_controller.rb
@@ -2,8 +2,9 @@ class Services::GovboxApiController < ContentController
   layout 'application_bs4'
 
   before_action :ensure_live, only: [:index]
+  before_action :set_up_footer_template
 
-    def index
+  def index
     @page.title = 'Správy zo slovensko.sk integrované na interný systém &middot; GovBox API'.html_safe
     @page.og.image = view_context.image_url('services/govbox_api/facebook_share.png')
     @page.og.description = 'Ideálne riešenie pre firmy, ktoré intenzívne využívajú schránku na slovensko.sk, potrebujú zasielať hromadné podania alebo uvažujú o automatizácii procesov. Od Slovensko.Digital.'
@@ -20,5 +21,9 @@ class Services::GovboxApiController < ContentController
     unless rollout.active?(:govbox)
       render template: 'errors/not_found', status: 404, layout: 'application'
     end
+  end
+  
+  def set_up_footer_template
+    @footer_template = 'services/govbox/footer'
   end
 end
