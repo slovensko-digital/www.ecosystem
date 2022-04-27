a6977568a176a42382a2d9a8ded90f79e0fc703d
luciajanikova <19lucia99@gmail.com>
2022-04-27 22:37:07 +0200

    Add consultations social share image

--- app/controllers/services/consultations_controller.rb
+++ app/controllers/services/consultations_controller.rb
@@ -5,7 +5,7 @@ class Services::ConsultationsController < ContentController
 
   def index
     @page.title = 'Konzultačné služby a analýzy podľa vašich potrieb'.html_safe
-    # @page.og.image = view_context.image_url('services/consultations/facebook_share.png')
+    @page.og.image = view_context.image_url('services/consultations/facebook_share.png')
     @page.og.description = 'Konzultačné služby a analýzy podľa vašich potrieb a individuálneho zadania.'
   end
 
