627f00c0052e91672c4057eed6ada2d29c41db14
Tomáš Drga <tomasxdrga@gmail.com>
2023-07-26 14:43:28 +0200

    got rid of webdriver update in capybara.rb

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -1,5 +1,3 @@
-Webdrivers::Chromedriver.update
-
 Capybara.register_driver :chrome do |app|
   options = Selenium::WebDriver::Chrome::Options.new(options: { args: %w(headless disable-gpu disable-logging) })
   Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
