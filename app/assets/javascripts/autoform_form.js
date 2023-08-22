9f63be81eb56b9470fec91738765ea801adb8fcd
Tomáš Drga <tomasxdrga@gmail.com>
2023-08-22 18:22:35 +0200

    Changed alert closing and status check

--- app/assets/javascripts/autoform_form.js
+++ app/assets/javascripts/autoform_form.js
@@ -21,24 +21,57 @@ $(document).ready(function() {
         }
         else {
             const formData = $(this).serialize();
-            $.post($(this).attr('action'), formData, function(response) {
 
-            }).complete(function(jqxhr) {
-                if (jqxhr.status === 200) {
+            $.post($(this).attr('action'), formData)
+                .done(function() {
                     $('#autoform-error').hide();
                     $('#autoform-form-error').hide();
                     $('#autoform-email').parent('.form-group').removeClass('has-error');
                     $('#autoform-domain').parent('.form-group').removeClass('has-error');
                     $('#autoform-form').hide();
                     $('#autoform-form-sent').show();
-
-                } else {
+                })
+                .fail(function() {
                     $('#autoform-form-error').show();
-                }
-            });
+                });
 
             plausible('Registration', {props: {product: 'Autoform'}});
             return true;
         }
     });
+
+    const wrapper = document.getElementById('flash-message-wrapper')
+    wrapper.addEventListener('click', (event) => {
+        const isButton = event.target.nodeName === 'SPAN';
+        if (!isButton) {return;}
+
+        $('#autoform-email').parent('.form-group').removeClass('has-error');
+        $('#autoform-domain').parent('.form-group').removeClass('has-error');
+
+        if(event.target.className === 'error') {
+            $('#autoform-error').hide();
+        }
+        else if(event.target.className === 'form-error') {
+            $('#autoform-form-error').hide();
+        }
+        else if(event.target.className === 'success') {
+            $('#autoform-form-sent').hide();
+        }
+    });
+
+    const emailInputField = document.getElementById('autoform-email');
+    emailInputField.addEventListener('click', removeErrors);
+
+    const domainInputField = document.getElementById('autoform-domain');
+    domainInputField.addEventListener('click', removeErrors);
 });
+
+function removeErrors() {
+    $('#autoform-email').parent('.form-group').removeClass('has-error');
+    $('#autoform-domain').parent('.form-group').removeClass('has-error');
+    $('#autoform-error').hide();
+    $('#autoform-form-error').hide();
+}
+
+
+
